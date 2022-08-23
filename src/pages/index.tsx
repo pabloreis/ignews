53e4df9bd11b6b1c5458f202d80e08302b215fb8
Pablo Reis <pablo.reis@storyteq.com>
2022-08-24 00:26:27 +0200

    feat: create home styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,7 @@
 import Head from 'next/head';
+import Image from 'next/image';
+
+import styles from './home.module.scss';
 
 export default function Home() {
   return (
@@ -6,6 +9,22 @@ export default function Home() {
       <Head>
         <title>Home | ig.news</title>
       </Head>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+
+          <p>
+            Get access to all the publications <br /> <span>$9.90 month</span>
+          </p>
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   );
 }
