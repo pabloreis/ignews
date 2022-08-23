3534fb8bdd957106cba1cf3afa519eeaed303e4d
Pablo Reis <pablo.reis@storyteq.com>
2022-08-23 23:52:48 +0200

    feat: add SignIn component

---
+++ src/components/SignInButton/index.tsx
@@ -0,0 +1,19 @@
+import { FaGithub } from 'react-icons/fa';
+import { FiX } from 'react-icons/fi';
+
+import styles from './styles.module.scss';
+
+export function SignInButton() {
+  const isUserLoggedIn = true;
+  const buttonColor = isUserLoggedIn ? '#84d361' : '#eba417';
+
+  return (
+    <button type="button" className={styles.signInButton}>
+      <FaGithub color={buttonColor} />
+
+      {isUserLoggedIn ? 'Pablo Reis' : 'Sign in with Github'}
+
+      {isUserLoggedIn && <FiX color="#737380" className={styles.closeIcon} />}
+    </button>
+  );
+}
