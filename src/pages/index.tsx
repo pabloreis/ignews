4ae5839a3e3f4c435af9526974e214fc766fee85
Pablo Reis <pablo.reis@storyteq.com>
2022-08-23 23:53:22 +0200

    refactor: replace PT by EN in home file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,10 +4,8 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Início | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
-
-      <h1>Hello World!</h1>
     </>
   );
 }
