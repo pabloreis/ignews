c552078c022551e6e7dd1a05164fb8f7c2c8f8c6
Pablo Reis <pablo.reis@storyteq.com>
2022-08-24 15:58:27 +0200

    feat: add signIn/signOut handlers

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,17 +1,24 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
+import { signIn, signOut, useSession } from 'next-auth/react';
 
 import styles from './styles.module.scss';
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  const { data: session, status } = useSession();
+
+  const isUserLoggedIn = status === 'authenticated';
   const buttonColor = isUserLoggedIn ? '#84d361' : '#eba417';
 
   return (
-    <button type="button" className={styles.signInButton}>
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => (isUserLoggedIn ? signOut() : signIn('github'))}
+    >
       <FaGithub color={buttonColor} />
 
-      {isUserLoggedIn ? 'Pablo Reis' : 'Sign in with Github'}
+      {isUserLoggedIn ? session?.user?.name : 'Sign in with Github'}
 
       {isUserLoggedIn && <FiX color="#737380" className={styles.closeIcon} />}
     </button>
