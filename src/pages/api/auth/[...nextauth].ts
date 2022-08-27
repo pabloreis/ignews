457c5a98f17d975a4280265a0452f82450e26d1f
Pablo Reis <pablo.reis@storyteq.com>
2022-08-27 22:47:20 +0200

    refactor: replace strings by Fauna enum

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,7 @@
 import { query as q } from 'faunadb';
 import NextAuth from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
+import { FaunaCollections, FaunaIndexes } from '../../../models/FaunaModel';
 
 import { fauna } from '../../../services/fauna';
 
@@ -25,11 +26,16 @@ export default NextAuth({
           q.If(
             q.Not(
               q.Exists(
-                q.Match(q.Index('user_by_email'), q.Casefold(user.email))
+                q.Match(
+                  q.Index(FaunaIndexes.UserByEmail),
+                  q.Casefold(user.email)
+                )
               )
             ),
-            q.Create(q.Collection('users'), { data: { email } }),
-            q.Get(q.Match(q.Index('user_by_email'), q.Casefold(user.email)))
+            q.Create(q.Collection(FaunaCollections.Users), { data: { email } }),
+            q.Get(
+              q.Match(q.Index(FaunaIndexes.UserByEmail), q.Casefold(user.email))
+            )
           )
         );
 
