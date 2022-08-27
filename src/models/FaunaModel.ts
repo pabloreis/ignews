89a4447498c9d2d8fbe554dc4ae62b7a4f516c49
Pablo Reis <pablo.reis@storyteq.com>
2022-08-27 22:31:20 +0200

    chore: create FaunaModel file

---
+++ src/models/FaunaModel.ts
@@ -0,0 +1,9 @@
+export enum FaunaCollections {
+  Subscriptions = 'subscriptions',
+  Users = 'users',
+}
+
+export enum FaunaIndexes {
+  UserByEmail = 'user_by_email',
+  UserByStripeCustomerId = 'user_by_stripe_custumer_id',
+}
